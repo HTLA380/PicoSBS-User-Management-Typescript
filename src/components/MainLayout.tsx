1798318ad842eb4efac335c10a7482f29983e468
Htet Aung Lin <htetlin099798@gmail.com>
2024-03-04 23:11:59 +0630

    chore: change favicon icon

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -24,7 +24,6 @@ const MainLayout: React.FC<MainLayoutInterface> = ({ children }) => {
         <title>ERPPOS</title>
         <meta name="description" content="ERPPOS" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="relative flex min-h-screen font-medium bg-background text-foreground">
